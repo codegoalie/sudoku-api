1376dc0091607e8be4cc608badd6f7385bb83101
Chris Marshall <chris@chrismar035.com>
2016-10-05 17:39:59 -0400

    Minimal gRPC client/server
    Server only returns one puzzle from an in-memory "repository" and the
    client only asks for that puzzle by UUID.

---
+++ client/client.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/codegoalie/sudoku-api/sudokuapi"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:10000"
+	id      = "3430eb79-7709-4957-bc63-0ceb650c4e6e"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewPuzzlerClient(conn)
+	resp, err := client.GetPuzzle(context.Background(), &pb.PuzzleID{Uuid: id})
+	if err != nil {
+		log.Fatalf("Failed to get puzzle: %v", err)
+	}
+
+	log.Printf("Here's the pzzle: %v", resp)
+}
---
+++ server/server.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+
+	pb "github.com/codegoalie/sudoku-api/sudokuapi"
+)
+
+var (
+	port         = flag.Int("port", 10000, "The server port")
+	repoInstance = staticRepo{db: map[string][]uint32{
+		"3430eb79-7709-4957-bc63-0ceb650c4e6e": []uint32{
+			7, 5, 0, 0, 0, 0, 0, 2, 0,
+			1, 0, 0, 2, 0, 0, 0, 0, 0,
+			3, 0, 0, 0, 9, 0, 4, 0, 6,
+			0, 0, 0, 1, 7, 0, 0, 0, 0,
+			0, 0, 1, 0, 3, 0, 5, 0, 0,
+			0, 0, 0, 0, 4, 8, 0, 0, 0,
+			8, 0, 9, 0, 5, 0, 0, 0, 2,
+			0, 0, 0, 0, 0, 7, 0, 0, 3,
+			0, 6, 0, 0, 0, 0, 0, 5, 1,
+		},
+	},
+	}
+)
+
+type staticRepo struct {
+	db map[string][]uint32
+}
+
+type server struct{}
+
+type repo interface {
+	GetSudoku(string) ([]uint32, error)
+}
+
+func (r staticRepo) GetSudoku(uuid string) ([]uint32, error) {
+	if grid, ok := r.db[uuid]; ok {
+		return grid, nil
+	}
+
+	return []uint32{}, errors.New("Unknown puzzle UUID")
+}
+
+func (s server) GetPuzzle(ctx context.Context, params *pb.PuzzleID) (*pb.Puzzle, error) {
+	fmt.Println(params.Uuid)
+	grid, err := repoInstance.GetSudoku(params.Uuid)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Puzzle{Uuid: params.Uuid, Cell: grid}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		grpclog.Fatalf("failed to listen: %v", *port)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterPuzzlerServer(grpcServer, server{})
+	grpcServer.Serve(lis)
+}
